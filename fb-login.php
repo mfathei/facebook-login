3f5461a6eb5bcd2e9f38b02834efb7d137858712
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-10-13 23:01:24 +0200

    init app

---
+++ fb-login.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => ,
+    'app_secret' => ,
+    'defaul_graph_version' => 'v2.10',
+    // 'default_access_token' => {access_token}
+]);
